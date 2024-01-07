f109ff72f39a030430b3b7118f4c9168121acf7e
Vladislav Antonov <vladislav.antonov@gmail.com>
2024-01-07 15:33:38 +0200

    Separate RGB image warping

--- refactored_code/include/ProjectiveMotionRL.h
+++ refactored_code/include/ProjectiveMotionRL.h
@@ -12,6 +12,11 @@ void warpImage(float* InputImg, float* inputWeight, int iwidth, int iheight,
                float* OutputImg, float* outputWeight, int width, int height,
                const Homography& homography);
 
+void warpImage(float* InputImgR, float* InputImgG, float* InputImgB,
+               float* inputWeight, int iwidth, int iheight, float* OutputImgR,
+               float* OutputImgG, float* OutputImgB, float* outputWeight,
+               int width, int height, const Homography& homography);
+
 class ProjectiveMotionRL {
  public:
   constexpr static int NumSamples = 30;
--- refactored_code/src/ProjectiveMotionRL.cpp
+++ refactored_code/src/ProjectiveMotionRL.cpp
@@ -46,6 +46,47 @@ void warpImage(float* InputImg, float* inputWeight, int iwidth, int iheight,
   }
 }
 
+void warpImage(float* InputImgR, float* InputImgG, float* InputImgB,
+               float* inputWeight, int iwidth, int iheight, float* OutputImgR,
+               float* OutputImgG, float* OutputImgB, float* outputWeight,
+               int width, int height, const Homography& homography) {
+  const float woffset = width * 0.5f;
+  const float hoffset = height * 0.5f;
+  const float iwoffset = iwidth * 0.5f;
+  const float ihoffset = iheight * 0.5f;
+
+  for (int y = 0, index = 0; y < height; y++) {
+    for (int x = 0; x < width; x++, index++) {
+      float fx = x - woffset;
+      float fy = y - hoffset;
+      homography.Transform(fx, fy);  // Inverse mapping, use inverse instead
+      fx += iwoffset;
+      fy += ihoffset;
+
+      if (fx >= 0 && fx < iwidth - 1 && fy >= 0 && fy < iheight - 1) {
+        if (inputWeight) {
+          outputWeight[index] =
+              0.01f +
+              ReturnInterpolatedValueFast(fx, fy, inputWeight, iwidth, iheight);
+        } else {
+          outputWeight[index] = 1.01f;
+        }
+      } else {
+        outputWeight[index] = 0.01f;
+      }
+
+      if (fx < 0) fx = 0;
+      if (fy < 0) fy = 0;
+      if (fx >= iwidth - 1.001f) fx = iwidth - 1.001f;
+      if (fy >= iheight - 1.001f) fy = iheight - 1.001f;
+
+      ReturnInterpolatedValueFast(fx, fy, InputImgR, InputImgG, InputImgB,
+                                  iwidth, iheight, OutputImgR[index],
+                                  OutputImgG[index], OutputImgB[index]);
+    }
+  }
+}
+
 float ProjectiveMotionRL::getSpsWeight(float aValue) const {
   if (!std::isfinite(aValue)) {
     return 0.0f;
@@ -73,76 +114,14 @@ void ProjectiveMotionRL::WarpImage(float* InputImgR, float* InputImgG,
                                    float* OutputImgG, float* OutputImgB,
                                    float* outputWeight, int width, int height,
                                    int i) {
-  int x = 0, y = 0, index = 0;
-  float fx = NAN, fy = NAN;
   if (i >= 0 && i < NumSamples) {
-    float woffset = width * 0.5f, hoffset = height * 0.5f;
-    float iwoffset = iwidth * 0.5f, ihoffset = iheight * 0.5f;
-
-    for (y = 0, index = 0; y < height; y++) {
-      for (x = 0; x < width; x++, index++) {
-        fx = x - woffset;
-        fy = y - hoffset;
-        IHmatrix[i].Transform(fx, fy);  // Inverse mapping, use inverse instead
-        fx += iwoffset;
-        fy += ihoffset;
-
-        if (fx >= 0 && fx < iwidth - 1 && fy >= 0 && fy < iheight - 1) {
-          if (inputWeight) {
-            outputWeight[index] =
-                0.01f + ReturnInterpolatedValueFast(fx, fy, inputWeight, iwidth,
-                                                    iheight);
-          } else {
-            outputWeight[index] = 1.01f;
-          }
-        } else {
-          outputWeight[index] = 0.01f;
-        }
-
-        if (fx < 0) fx = 0;
-        if (fy < 0) fy = 0;
-        if (fx >= iwidth - 1.001f) fx = iwidth - 1.001f;
-        if (fy >= iheight - 1.001f) fy = iheight - 1.001f;
-
-        ReturnInterpolatedValueFast(fx, fy, InputImgR, InputImgG, InputImgB,
-                                    iwidth, iheight, OutputImgR[index],
-                                    OutputImgG[index], OutputImgB[index]);
-      }
-    }
+    warpImage(InputImgR, InputImgG, InputImgB, inputWeight, iwidth, iheight,
+              OutputImgR, OutputImgG, OutputImgB, outputWeight, width, height,
+              IHmatrix[i]);
   } else if (i < 0 && i > -NumSamples) {
-    float woffset = width * 0.5f, hoffset = height * 0.5f;
-    float iwoffset = iwidth * 0.5f, ihoffset = iheight * 0.5f;
-
-    for (y = 0, index = 0; y < height; y++) {
-      for (x = 0; x < width; x++, index++) {
-        fx = x - woffset;
-        fy = y - hoffset;
-        Hmatrix[-i].Transform(fx, fy);  // Inverse mapping, use inverse instead
-        fx += iwoffset;
-        fy += ihoffset;
-
-        if (fx >= 0 && fx < iwidth - 1 && fy >= 0 && fy < iheight - 1) {
-          if (inputWeight) {
-            outputWeight[index] =
-                0.01f + ReturnInterpolatedValueFast(fx, fy, inputWeight, iwidth,
-                                                    iheight);
-          } else {
-            outputWeight[index] = 1.01f;
-          }
-        } else {
-          outputWeight[index] = 0.01f;
-        }
-
-        if (fx < 0) fx = 0;
-        if (fy < 0) fy = 0;
-        if (fx >= iwidth - 1.001f) fx = iwidth - 1.001f;
-        if (fy >= iheight - 1.001f) fy = iheight - 1.001f;
-
-        ReturnInterpolatedValueFast(fx, fy, InputImgR, InputImgG, InputImgB,
-                                    iwidth, iheight, OutputImgR[index],
-                                    OutputImgG[index], OutputImgB[index]);
-      }
-    }
+    warpImage(InputImgR, InputImgG, InputImgB, inputWeight, iwidth, iheight,
+              OutputImgR, OutputImgG, OutputImgB, outputWeight, width, height,
+              Hmatrix[-i]);
   }
 }
 
