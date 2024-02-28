52e52713f04790f9e0dac5c94c68620d7cff1c12
Vladislav Antonov <vladislav.antonov@gmail.com>
2024-02-28 20:31:57 +0200

    Initial BilateralRegularizer

--- refactored_code/include/RLDeblurrerBilateralReg.hpp
+++ refactored_code/include/RLDeblurrerBilateralReg.hpp
@@ -4,6 +4,7 @@
 
 #include "IBlurImageGenerator.hpp"
 #include "IErrorCalculator.hpp"
+#include "IRegularizer.hpp"
 
 class RLDeblurrerBilateralReg {
  public:
@@ -62,4 +63,46 @@ class RLDeblurrerBilateralReg {
   std::vector<float> mErrorImgBufferB;
 
   std::vector<float> mErrorWeightBuffer;
+};
+
+// We use the same lambda as in TV regularization for better comparison
+// Parameter setting, noise variance, for bilateral reg is the same as
+// laplacian reg.
+class BilateralRegularizer : public IRegularizer {
+ public:
+  BilateralRegularizer();
+  ~BilateralRegularizer() override { ClearBuffer(); }
+
+  ////////////////////////////////////
+  // These functions are used to set Buffer for caching
+  ////////////////////////////////////
+  void SetBuffer(int width, int height);
+  void ClearBuffer();
+
+  ////////////////////////////////////
+  // These functions are used to apply regularization
+  ////////////////////////////////////
+  void applyRegularizationGray(float* DeblurImg, int width, int height,
+                               bool bPoisson, float lambda) override;
+
+  void applyRegularizationRgb(float* DeblurImgR, float* DeblurImgG,
+                              float* DeblurImgB, int width, int height,
+                              bool bPoisson, float lambda) override;
+
+ private:
+  void SetBilateralTable();
+
+  // These are buffer and lookup table variables
+  float mBilateralTable[256]{};
+
+  ////////////////////////////////////
+  // These functions are used to compute derivatives for regularization
+  ////////////////////////////////////
+  void ComputeBilaterRegImageGray(float* Img, int width, int height,
+                                  float* BRImg);
+
+  std::vector<float> mBilateralRegImg;
+  std::vector<float> mBilateralRegImgR;
+  std::vector<float> mBilateralRegImgG;
+  std::vector<float> mBilateralRegImgB;
 };
