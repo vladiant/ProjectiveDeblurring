58d0fe49abe6a8ac792f5c13b83e1e51abc8d2a4
Vladislav Antonov <vladislav.antonov@gmail.com>
2024-03-18 07:59:36 +0200

    Add blur algorithm selection

--- refactored_code/ProjectiveMotionRL_main.cpp
+++ refactored_code/ProjectiveMotionRL_main.cpp
@@ -1,3 +1,4 @@
+#include <charconv>
 #include <cstring>
 #include <fstream>
 #include <iomanip>
@@ -21,7 +22,7 @@
 
 int main(int argc, char* argv[]) {
   if (argc < 2) {
-    printf("Usage: %s image_filename\n", argv[0]);
+    printf("Usage: %s image_filename [blur_type]\n", argv[0]);
     return EXIT_SUCCESS;
   }
 
@@ -32,11 +33,11 @@ int main(int argc, char* argv[]) {
     printf("Expected %s to end with %s\n", fname.c_str(), fileExtension);
     return EXIT_SUCCESS;
   }
-  std::string prefix = fname.substr(0, pos);
+  const auto prefix = fname.substr(0, pos);
 
   int width = 0, height = 0;
   std::vector<float> fImg[3];
-  // memcpy(prefix, argv[1], strlen(argv[1])+1);
+
   printf("Load Image: %s\n", fname.c_str());
   readBMPchannels(fname, fImg[0], fImg[1], fImg[2], width, height);
   int blurwidth = width, blurheight = height;
@@ -68,78 +69,112 @@ int main(int argc, char* argv[]) {
   RMSErrorCalculator errorCalculator;
   EmptyErrorCalculator emptyErrorCalculator;
 
-  blurGenerator.SetGlobalParameters(  //
-      10,                             // rotation degree
-      1.2f,                           // scale factor
-      0.0003f,                        // perspective step x
-      0.0006f,                        // perspective step y
-      10,                             // translation step x
-      20                              // translation step y
-  );  // This is parameter setting for doll example
-
-  //   blurGenerator.SetGlobalParameters(  //
-  //       -10,                            // rotation degree
-  //       1.1f,                           // scale factor
-  //       0.0004f,                        //
-  //       0.0002f,                        //
-  //       20,                             //
-  //       -15                             //
-  //   );  // This is parameter setting for Cameraman convergence example
-
-  // Testing case for rotational motion
-  // blurGenerator.SetGlobalRotation(10);
-
-  // Testing case for zooming motion
-  // blurGenerator.SetGlobalScaling(1.2f);
-
-  // Testing case for translational motion
-  if (false) {
-    float deltadx = 0.8f;
-    for (int i = 0; i < MotionBlurImageGenerator::NumSamples; i++) {
-      float dy = 5.0f * sin((float)(i) / MotionBlurImageGenerator::NumSamples *
-                            2 * M_PI);
-      blurGenerator.Hmatrix[i].Hmatrix[0][0] = 1;
-      blurGenerator.Hmatrix[i].Hmatrix[0][1] = 0;
-      blurGenerator.Hmatrix[i].Hmatrix[0][2] = i * deltadx;
-      blurGenerator.Hmatrix[i].Hmatrix[1][0] = 0;
-      blurGenerator.Hmatrix[i].Hmatrix[1][1] = 1;
-      blurGenerator.Hmatrix[i].Hmatrix[1][2] = dy;
-      blurGenerator.Hmatrix[i].Hmatrix[2][0] = 0;
-      blurGenerator.Hmatrix[i].Hmatrix[2][1] = 0;
-      blurGenerator.Hmatrix[i].Hmatrix[2][2] = 1;
-      Homography::MatrixInverse(blurGenerator.Hmatrix[i].Hmatrix,
-                                blurGenerator.IHmatrix[i].Hmatrix);
+  int blurType = 0;
+  if (argc > 2) {
+    const std::string blurTypeArg{argv[2]};
+    const auto convResult = std::from_chars(
+        blurTypeArg.data(), blurTypeArg.data() + blurTypeArg.size(), blurType);
+    if (convResult.ec != std::errc()) {
+      printf("Error convering %s to int\n", blurTypeArg.c_str());
+      return EXIT_SUCCESS;
     }
   }
 
-  // Testing case for projective motion
-  // blurGenerator.SetGlobalPerspective(0.001f,0.001f);
+  switch (blurType) {
+    case 0:
+      printf("Doll example blurring\n");
+      blurGenerator.SetGlobalParameters(  //
+          10,                             // rotation degree
+          1.2f,                           // scale factor
+          0.0003f,                        // perspective step x
+          0.0006f,                        // perspective step y
+          10,                             // translation step x
+          20                              // translation step y
+      );  // This is parameter setting for doll example
+      break;
+    case 1:
+      printf("Cameraman convergence example blurring\n");
+      blurGenerator.SetGlobalParameters(  //
+          -10,                            // rotation degree
+          1.1f,                           // scale factor
+          0.0004f,                        //
+          0.0002f,                        //
+          20,                             //
+          -15                             //
+      );  // This is parameter setting for Cameraman convergence example
+      break;
+    case 2:
+      printf("Rotational motion example blurring\n");
+      //   Testing case for rotational motion
+      blurGenerator.SetGlobalRotation(10);
+      break;
+    case 3:
+      printf("Zooming motion example blurring\n");
+      // Testing case for zooming motion
+      blurGenerator.SetGlobalScaling(1.2f);
+      break;
+    case 4:
+      printf("Translational motion example blurring\n");
+      // Testing case for translational motion
+      {
+        float deltadx = 0.8f;
+        for (int i = 0; i < MotionBlurImageGenerator::NumSamples; i++) {
+          float dy =
+              5.0f *
+              sin((float)(i) / MotionBlurImageGenerator::NumSamples * 2 * M_PI);
+          blurGenerator.Hmatrix[i].Hmatrix[0][0] = 1;
+          blurGenerator.Hmatrix[i].Hmatrix[0][1] = 0;
+          blurGenerator.Hmatrix[i].Hmatrix[0][2] = i * deltadx;
+          blurGenerator.Hmatrix[i].Hmatrix[1][0] = 0;
+          blurGenerator.Hmatrix[i].Hmatrix[1][1] = 1;
+          blurGenerator.Hmatrix[i].Hmatrix[1][2] = dy;
+          blurGenerator.Hmatrix[i].Hmatrix[2][0] = 0;
+          blurGenerator.Hmatrix[i].Hmatrix[2][1] = 0;
+          blurGenerator.Hmatrix[i].Hmatrix[2][2] = 1;
+          Homography::MatrixInverse(blurGenerator.Hmatrix[i].Hmatrix,
+                                    blurGenerator.IHmatrix[i].Hmatrix);
+        }
+      }
+      break;
+    case 5:
+      printf("Projective motion example blurring\n");
+      // Testing case for projective motion
+      blurGenerator.SetGlobalPerspective(0.001f, 0.001f);
+      break;
+    case 6:
+      printf("Case 1-4 example blurring\n");
+      // Adjust the parameters for generating test case 1-15
+      {
+        float deltadx = 0.8f;
+        float deltascaling = 0.2f / MotionBlurImageGenerator::NumSamples;
+        float deltapx = 0.001f / MotionBlurImageGenerator::NumSamples;
+        float deltapy = 0.001f / MotionBlurImageGenerator::NumSamples;
+        float deltadegree =
+            (10.0f * M_PI / 180.0f) / MotionBlurImageGenerator::NumSamples;
+        for (int i = 0; i < MotionBlurImageGenerator::NumSamples; i++) {
+          float dy =
+              5.0f *
+              sin((float)(i) / MotionBlurImageGenerator::NumSamples * 2 * M_PI);
+          blurGenerator.Hmatrix[i].Hmatrix[0][0] =
+              (1 + i * deltascaling) * cos(deltadegree * i);
+          blurGenerator.Hmatrix[i].Hmatrix[0][1] = sin(deltadegree * i);
+          blurGenerator.Hmatrix[i].Hmatrix[0][2] = i * deltadx;
+          blurGenerator.Hmatrix[i].Hmatrix[1][0] = -sin(deltadegree * i);
+          blurGenerator.Hmatrix[i].Hmatrix[1][1] =
+              (1 + i * deltascaling) * cos(deltadegree * i);
+          blurGenerator.Hmatrix[i].Hmatrix[1][2] = dy;
+          blurGenerator.Hmatrix[i].Hmatrix[2][0] = i * deltapx;
+          blurGenerator.Hmatrix[i].Hmatrix[2][1] = i * deltapy;
+          blurGenerator.Hmatrix[i].Hmatrix[2][2] = 1;
+          Homography::MatrixInverse(blurGenerator.Hmatrix[i].Hmatrix,
+                                    blurGenerator.IHmatrix[i].Hmatrix);
+        }
+      }
+      break;
 
-  // Adjust the parameters for generating test case 1-15
-  if (false) {
-    float deltadx = 0.8f;
-    float deltascaling = 0.2f / MotionBlurImageGenerator::NumSamples;
-    float deltapx = 0.001f / MotionBlurImageGenerator::NumSamples;
-    float deltapy = 0.001f / MotionBlurImageGenerator::NumSamples;
-    float deltadegree =
-        (10.0f * M_PI / 180.0f) / MotionBlurImageGenerator::NumSamples;
-    for (int i = 0; i < MotionBlurImageGenerator::NumSamples; i++) {
-      float dy = 5.0f * sin((float)(i) / MotionBlurImageGenerator::NumSamples *
-                            2 * M_PI);
-      blurGenerator.Hmatrix[i].Hmatrix[0][0] =
-          (1 + i * deltascaling) * cos(deltadegree * i);
-      blurGenerator.Hmatrix[i].Hmatrix[0][1] = sin(deltadegree * i);
-      blurGenerator.Hmatrix[i].Hmatrix[0][2] = i * deltadx;
-      blurGenerator.Hmatrix[i].Hmatrix[1][0] = -sin(deltadegree * i);
-      blurGenerator.Hmatrix[i].Hmatrix[1][1] =
-          (1 + i * deltascaling) * cos(deltadegree * i);
-      blurGenerator.Hmatrix[i].Hmatrix[1][2] = dy;
-      blurGenerator.Hmatrix[i].Hmatrix[2][0] = i * deltapx;
-      blurGenerator.Hmatrix[i].Hmatrix[2][1] = i * deltapy;
-      blurGenerator.Hmatrix[i].Hmatrix[2][2] = 1;
-      Homography::MatrixInverse(blurGenerator.Hmatrix[i].Hmatrix,
-                                blurGenerator.IHmatrix[i].Hmatrix);
-    }
+    default:
+      printf("Unknown case: %d\n", blurType);
+      return EXIT_SUCCESS;
   }
 
   ///////////////////////////////////
