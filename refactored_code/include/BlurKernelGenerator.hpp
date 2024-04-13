c33ddd826f31ffd1d3fe4522afe879592d24d729
Vladislav Antonov <vladislav.antonov@gmail.com>
2024-04-13 20:04:16 +0300

    Add BlurKernelGenerator fields

--- refactored_code/include/BlurKernelGenerator.hpp
+++ refactored_code/include/BlurKernelGenerator.hpp
@@ -15,6 +15,9 @@ class BlurKernelGenerator : public IBlurImageGenerator {
  public:
   enum class Border { ISOLATED, REPLICATE, REFLECT, WRAP };
 
+  BlurKernelGenerator(int aKernelWidth, int aKernelHeight, Border aBorder,
+                      float* aBaseImg, int aWidth, int aHeight);
+
   // bforward: true forward, false backward
   void blurGray(float* InputImg, float* inputWeight, int iwidth, int iheight,
                 float* BlurImg, float* outputWeight, int width, int height,
@@ -26,4 +29,13 @@ class BlurKernelGenerator : public IBlurImageGenerator {
 
   void SetBuffer(int width, int height) override;
   void ClearBuffer() override;
+
+ private:
+  int mKernelWidth{};
+  int mKernelHeight{};
+  Border mBorder{};
+
+  float* mBaseImg{};
+  int mWidth{};
+  int mHeight{};
 };
--- refactored_code/src/BlurKernelGenerator.cpp
+++ refactored_code/src/BlurKernelGenerator.cpp
@@ -1,5 +1,15 @@
 #include "BlurKernelGenerator.hpp"
 
+BlurKernelGenerator::BlurKernelGenerator(int aKernelWidth, int aKernelHeight,
+                                         Border aBorder, float* aBaseImg,
+                                         int aWidth, int aHeight)
+    : mKernelWidth{aKernelWidth},
+      mKernelHeight{aKernelHeight},
+      mBorder{aBorder},
+      mBaseImg{aBaseImg},
+      mWidth{aWidth},
+      mHeight{aHeight} {}
+
 void BlurKernelGenerator::blurGray(float* InputImg, float* inputWeight,
                                    int iwidth, int iheight, float* BlurImg,
                                    float* outputWeight, int width, int height,
@@ -12,6 +22,7 @@ void BlurKernelGenerator::blurRgb(float* InputImgR, float* InputImgG,
                                   float* outputWeight, int width, int height,
                                   bool bforward) {}
 
-void BlurKernelGenerator::SetBuffer(int width, int height) {}
+void BlurKernelGenerator::SetBuffer([[maybe_unused]] int width,
+                                    [[maybe_unused]] int height) {}
 
 void BlurKernelGenerator::ClearBuffer() {}
