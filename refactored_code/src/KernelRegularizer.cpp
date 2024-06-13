ef5c675a93bc8be399117ba5671eede33b70e61b
Vladislav Antonov <vladislav.antonov@gmail.com>
2024-06-13 20:58:40 +0300

    Add color BlurKernel regularizer

--- refactored_code/src/KernelRegularizer.cpp
+++ refactored_code/src/KernelRegularizer.cpp
@@ -34,4 +34,48 @@ void KernelRegularizer::applyRegularizationRgb(float* DeblurImgR,
                                                float* DeblurImgG,
                                                float* DeblurImgB, int width,
                                                int height, bool bPoisson,
-                                               float lambda) {}
+                                               float lambda) {
+  // Sum of abs weight should be one
+  // There can not be negative weight
+  float sumR = 0;
+  float sumG = 0;
+  float sumB = 0;
+  for (int index = 0; index < width * height; index++) {
+    if (DeblurImgR[index] > 0) {
+      sumR += DeblurImgR[index];
+    } else {
+      DeblurImgR[index] = 0;
+    }
+
+    if (DeblurImgG[index] > 0) {
+      sumG += DeblurImgG[index];
+    } else {
+      DeblurImgG[index] = 0;
+    }
+
+    if (DeblurImgB[index] > 0) {
+      sumB += DeblurImgB[index];
+    } else {
+      DeblurImgB[index] = 0;
+    }
+  }
+
+  // TODO: fix comparison to zero
+  if (sumR == 0) {
+    return;
+  }
+
+  if (sumG == 0) {
+    return;
+  }
+
+  if (sumB == 0) {
+    return;
+  }
+
+  for (int index = 0; index < width * height; index++) {
+    DeblurImgR[index] /= sumR;
+    DeblurImgG[index] /= sumG;
+    DeblurImgB[index] /= sumB;
+  }
+}
